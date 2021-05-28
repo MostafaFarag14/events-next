c2da38f58a9434761d2a69e78294c2bd1d87f04e
mostafafarag <mostafafarag140@gmail.com>
2021-05-28 17:43:58 +0200

    fix databases uri

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 export async function connectDatabase() {
   const client = await MongoClient.connect(
-    'mongodb+srv://maximilian:8ZO3ycZqJ23kWBQx@cluster0.ntrwp.mongodb.net/events?retryWrites=true&w=majority'
+    "mongodb+srv://mostafafarag:<Mf.361987>@cluster0.ea4jj.gcp.mongodb.net/events?retryWrites=true&w=majority"
   );
 
   return client;
